a6d1db2ab73c632bbe93cfd92041b258359abdbf
blahner <benlahner@gmail.com>
2025-06-10 16:06:30 -0400

    fixed filename typo

--- src/fmriDatasetPreparation/datasets/BOLDMomentsDataset/GLM/organize_betas.sh
+++ src/fmriDatasetPreparation/datasets/BOLDMomentsDataset/GLM/organize_betas.sh
@@ -3,7 +3,7 @@ set -e
 for subj in {01..10}; do
 echo "Starting glm estimation for sub-${subj}"
     echo "organizing betas"
-    python3 bmd_organize_betas_combined_testtrain.py -s ${subj}
+    python3 organize_betas.py -s ${subj}
     echo "Finished subject ${subj}"
 done
 echo "Finished organizing GLMs for all subjects in the loop"
